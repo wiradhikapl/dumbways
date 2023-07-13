b26a39d02c4311a85c8a46cf0e74ab7bfb018f1c
wiradhikapl <wiradhika.pl@gmailcom>
2023-07-13 19:05:20 +0700

    task

---
+++ Day6/js/Bars.js
@@ -0,0 +1,17 @@
+
+
+
+let barsIsOpen = false
+function barsopen() {
+    let barsContainer = document.getElementById("bars-container")
+    if (!barsIsOpen) {
+        barsContainer.style.display = "flex"
+        barsIsOpen = true;
+    } else {
+        barsContainer.style.display = "none"
+        barsIsOpen = false;
+    }
+}
+
+
+
